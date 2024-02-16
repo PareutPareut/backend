a5d8892319bdb5443431d606a8372874baa9eb40
xEzIxX <by1094@gachon.ac.kr>
2024-02-17 02:37:05 +0900

    refactor : 모델 연결 파일에서 연결 관계 추가

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,10 @@
 import { config } from '../config/config.js'
 import { Sequelize, DataTypes, Model } from 'sequelize'
+
 import { user } from '../models/user.js'
+import { userTime } from './userTime.js'
+import { event } from './event.js'
+import { eventDate } from './eventDate.js'
 
 const db = {} // 실제 데이터베이스가 이 db 객체와 연결됨
 
@@ -11,12 +15,14 @@ const sequelize = new Sequelize(
     config.options
 )
 
-db.sequelize = sequelize // 나중에 연결 객체 재사용을 위해 넣어둠, db객체에 sequelize라는 프로퍼티 추가
+db.sequelize = sequelize
 
-db.User = user(Model, sequelize, DataTypes)
+db.user = user(Model, sequelize, DataTypes)
+db.userTime = userTime(Model, sequelize, DataTypes)
 
-/*
-여기서 연관관계 설정
- */
+db.event = event(Model, sequelize, DataTypes)
+db.eventDate = eventDate(Model, sequelize, DataTypes)
 
-export { db } // export 하기
+//여기서 연관관계 설정
+
+export { db }
