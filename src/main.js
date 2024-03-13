66644335f5c35c609a92b36499ae0f8450df70af
xEzIxX <by1094@gachon.ac.kr>
2024-03-13 17:15:45 +0900

    chore : main.js 파일 위치 수정

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import express from 'express'
 import session from 'express-session'
 import morgan from 'morgan'
 import cors from 'cors'
-import { sequelizeLoader } from './src/loaders/sequelize.js'
+import { sequelizeLoader } from './loaders/sequelize.js'
 
-import { signUpRouter } from './src/controllers/signup.js'
-import { eventRouter } from './src/controllers/event.js'
+import { signUpRouter } from './controllers/signup.js'
+import { eventRouter } from './controllers/event.js'
 
 const app = express()
 
