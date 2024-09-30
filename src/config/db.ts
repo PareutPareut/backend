37ab3814e7914cd211213b74c7c2dff134e8de6a
xEzIxX <by1094@gachon.ac.kr>
2024-09-30 23:00:11 +0900

    refactor : dbConfig 구조 수정
    - options 객체 제거
    - host와 dialect를 상위 레벨로 이동하여 설정 간소화

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,8 +4,6 @@ export const dbConfig = {
   database: process.env.DB_NAME || "database",
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "password",
-  options: {
-    host: process.env.DB_HOST || "localhost",
-    dialect: process.env.DB_DIALECT,
-  },
+  host: process.env.DB_HOST || "localhost",
+  dialect: process.env.DB_DIALECT!,
 };
