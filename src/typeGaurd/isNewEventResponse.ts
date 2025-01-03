2dd35056555ff2a1570537b46e4449ba14c4a86a
정예지 <by1094@gachon.ac.kr>
2025-01-03 16:34:03 +0900

    chore : 사용되지 않는 typeGaurd 디렉토리 삭제

--- src/typeGaurd/isNewEventResponse.ts
+++
@@ -1,6 +0,0 @@
-import { ApiResponse } from "../interfaces/response/apiResponse";
-import { NewEventResponse } from "../interfaces/eventResponse";
-
-export function isNewEventResponse(response: ApiResponse): response is NewEventResponse {
-  return (response as NewEventResponse).eventId !== undefined;
-}
